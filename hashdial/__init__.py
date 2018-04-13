282c23f96aae02c3d4c4605b0affaf2606c82958
Peter Schuller <peter.schuller@infidyne.com>
2018-04-12 23:27:52 -0700

    Implement functions + partial test coverage. (#3)

--- hashdial/__init__.py
+++ hashdial/__init__.py
@@ -1,9 +1,17 @@
 """Implements a hash dial for hash based decision making.
 """
 import hashlib
+import math
+import sys
 
 from typing import TypeVar
 from typing import List
+from typing import Optional
+
+
+# Maximum integer representable exactly as a float.
+_MAX_FLOAT_REPRESENTABLE_INT = 2**(sys.float_info.mant_dig) - 1
+_MIN_FLOAT_REPRESENTABLE_INT = -_MAX_FLOAT_REPRESENTABLE_INT
 
 
 def _hfloat(b: bytes) -> float:
@@ -13,15 +21,30 @@ def _hfloat(b: bytes) -> float:
 
 
 def is_selected(probability: float, b: bytes) -> bool:
-    pass
+    if probability < 0.0:
+        raise ValueError('probability must be >= 0.0'.format(probability))
+    if probability > 1.0:
+        raise ValueError('probability must be <= 1.0'.format(probability))
+
+    return _hfloat(b) < probability
 
 
-def select_n(max_n: int, b: bytes) -> int:
-    pass
+def select_n(stop: int, b: bytes, *, start: Optional[int]=None) -> int:
+    if start is None:
+        start = 0
+
+    if stop <= start:
+        raise ValueError('stop ({}) must be > start ({})'.format(stop, start))
+
+    if start < _MIN_FLOAT_REPRESENTABLE_INT or stop > _MAX_FLOAT_REPRESENTABLE_INT:
+        raise ValueError('stop/start must be in range [{}, {}] due to limitations of floats',
+                         _MIN_FLOAT_REPRESENTABLE_INT, _MAX_FLOAT_REPRESENTABLE_INT)
+
+    return int(start + math.floor((stop - start) * _hfloat(b)))
 
 
 BucketType = TypeVar('BucketType')
 
 
 def select_bucket(buckets: List[BucketType], b: bytes) -> BucketType:
-    pass
+    return buckets[select_n(len(buckets), b)]
--- tests/test_hashdial.py
+++ tests/test_hashdial.py
@@ -1,6 +1,8 @@
-import hashdial
+import pytest
 
 from typing import Dict  # noqa (mypy/lint fight)
+
+import hashdial
 
 
 def test_hfloat_distribution() -> None:
@@ -10,6 +12,8 @@ def test_hfloat_distribution() -> None:
     buckets = {}  # type: Dict[int, int]
     for n in range(NUM_SAMPLES):
         f = hashdial._hfloat('{}'.format(n).encode('utf-8'))
+        assert f >= 0.0
+        assert f <= 1.0
         bucket = int(f * NUM_BUCKETS)
         buckets[bucket] = buckets.get(bucket, 0) + 1
 
@@ -17,3 +21,39 @@ def test_hfloat_distribution() -> None:
     for _, count in buckets.items():
         assert count > float(1) / NUM_BUCKETS * NUM_SAMPLES * 0.9
         assert count < float(1) / NUM_BUCKETS * NUM_SAMPLES * 1.1
+
+
+def test_is_selected_distribution() -> None:
+    PROBABILITY = 0.25
+    NUM_SAMPLES = 1000
+
+    value_count = {}  # type: Dict[bool,int]
+    for n in range(NUM_SAMPLES):
+        b = hashdial.is_selected(PROBABILITY, '{}'.format(n).encode('utf-8'))
+        value_count[b] = value_count.get(b, 0) + 1
+
+    assert value_count[True] > PROBABILITY * NUM_SAMPLES * 0.9
+    assert value_count[True] < PROBABILITY * NUM_SAMPLES * 1.1
+
+
+def test_select_n_distribution() -> None:
+    NUM_SAMPLES = 10000
+
+    values = {}  # type: Dict[int, int]
+
+    for n in range(NUM_SAMPLES):
+        selected = hashdial.select_n(2, '{}'.format(n).encode('utf-8'), start=-1)
+        values[selected] = values.get(selected, 0) + 1
+
+    assert set(values.keys()) == {-1, 0, 1}
+
+    for val in [-1, 0, 1]:
+        assert values[val] > NUM_SAMPLES * 0.33 * 0.9
+        assert values[val] < NUM_SAMPLES * 0.33 * 1.1
+
+
+def test_select_n_large_val() -> None:
+    with pytest.raises(ValueError):
+        hashdial.select_n(2**63, b'')
+    with pytest.raises(ValueError):
+        hashdial.select_n(-(2**63), b'')
