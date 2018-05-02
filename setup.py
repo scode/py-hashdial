20b0a76c3646ea72191646a12f720bb316cf6068
Peter Schuller <peter.schuller@infidyne.com>
2018-05-01 20:37:17 -0700

    Ready setup.py for an initial release attempt (maybe).

--- setup.py
+++ setup.py
@@ -8,8 +8,15 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='hashdial',
     version='0.1.0',
-    description='A module implementing a hash dial for hash based decision making',
-    long_description='TODO',
+    description='A module implementing hash based decision making',
+    long_description="""Implements, through hashing, decision making that is deterministic on input, but probabilistic across a set of inputs.
+
+For example, suppose a set of components in a distributed system wish to emit a log entry for 1% of requests - but each
+component should log the *same* 1% of requests, they could do so as such::
+
+    if hashdial.decide(request.id, 0.01):
+        log_request(request)
+""",
     long_description_content_type='text/x-rst',
     url='https://github.com/scode/py-hashdial',
     author='Peter Schuller',
@@ -20,21 +27,10 @@ setup(
     # For a list of valid classifiers, see
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -43,51 +39,15 @@ setup(
     keywords='hashdial',
     packages=find_packages(exclude=['docs', 'scripts', 'tests']),
 
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'typing>=3.6.0'
     ],
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+    extras_require={
+        # see requirements.in/requirements.txt
     },
 
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
-
-    # List additional URLs that are relevant to your project as a dict.
-    #
-    # This field corresponds to the "Project-URL" metadata fields:
-    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
-    #
-    # Examples listed include a pattern for specifying where the package tracks
-    # issues, where the source is hosted, where to say thanks to the package
-    # maintainers, and where to support the project financially. The key is
-    # what's used to render the link text on PyPI.
-    # project_urls={
-    #     'Documentation': 'TODO',
-    # },
+    project_urls={
+        'Documentation': 'http://py-hashdial.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/scode/py-hashdial',
+    },
 )
